481c7ef5f660ddae0ce7bf7686ad84c11dff83a3
Cris <cristina.romero.calvo@gmail.com>
2019-02-26 18:55:59 +0100

    Including GNSS folders and GnssCoreService all from GNSS compare.

---
+++ app/src/main/java/com/gnssis/rco/gnsstars_gnssisteam/GNSS_Constellations/GalileoGpsConstellation.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2018 TFI Systems
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.gnssis.rco.gnsstars_gnssisteam.GNSS_Constellations;
+
+import android.location.GnssMeasurementsEvent;
+import android.location.Location;
+
+import com.gnssis.rco.gnsstars_gnssisteam.GNSS_Corrections.Correction;
+import com.gnssis.rco.gnsstars_gnssisteam.GNSS_part.Coordinates;
+import com.gnssis.rco.gnsstars_gnssisteam.GNSS_part.Time;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GalileoGpsConstellation extends Constellation {
+
+    private GpsConstellation gpsConstellation = new GpsConstellation();
+    private GalileoConstellation galileoConstellation = new GalileoConstellation();
+
+    private static final String NAME = "Galileo + GPS";
+
+    /**
+     * List holding observed satellites
+     */
+    protected List<SatelliteParameters> observedSatellites = new ArrayList<>();
+    private List<SatelliteParameters> unusedSatellites = new ArrayList<>();
+
+    @Override
+    public Coordinates getRxPos() {
+        synchronized (this) {
+            return gpsConstellation.getRxPos();
+        }
+    }
+
+    @Override
+    public void setRxPos(Coordinates rxPos) {
+        synchronized (this) {
+            gpsConstellation.setRxPos(rxPos);
+            galileoConstellation.setRxPos(rxPos);
+        }
+    }
+
+    @Override
+    public SatelliteParameters getSatellite(int index) {
+        synchronized (this) {
+            return observedSatellites.get(index);
+        }
+    }
+
+    @Override
+    public List<SatelliteParameters> getSatellites() {
+        synchronized (this) {
+            return observedSatellites;
+        }
+    }
+
+    @Override
+    public List<SatelliteParameters> getUnusedSatellites() {
+        return unusedSatellites;
+    }
+
+    @Override
+    public int getVisibleConstellationSize() {
+        synchronized (this) {
+            return observedSatellites.size()+unusedSatellites.size();
+        }
+    }
+
+    @Override
+    public int getUsedConstellationSize() {
+        synchronized (this) {
+            return observedSatellites.size();
+        }
+    }
+
+    @Override
+    public void calculateSatPosition(Location location, Coordinates position) {
+        synchronized (this) {
+            gpsConstellation.calculateSatPosition(location, position);
+            galileoConstellation.calculateSatPosition(location, position);
+
+            observedSatellites.clear();
+            unusedSatellites.clear();
+
+            observedSatellites.addAll(gpsConstellation.getSatellites());
+            observedSatellites.addAll(galileoConstellation.getSatellites());
+
+            unusedSatellites.addAll(gpsConstellation.getUnusedSatellites());
+            unusedSatellites.addAll(galileoConstellation.getUnusedSatellites());
+        }
+    }
+
+    public void updateMeasurements(GnssMeasurementsEvent event) {
+        synchronized (this) {
+
+            observedSatellites.clear();
+            unusedSatellites.clear();
+
+            galileoConstellation.updateMeasurements(event);
+            gpsConstellation.updateMeasurements(event);
+
+        }
+    }
+
+    @Override
+    public double getSatelliteSignalStrength(int index) {
+        synchronized (this) {
+            return observedSatellites.get(index).getSignalStrength();
+        }
+    }
+
+    @Override
+    public int getConstellationId() {
+        synchronized (this) {
+            return Constellation.CONSTELLATION_GALILEO_GPS;
+        }
+    }
+
+    @Override
+    public void addCorrections(ArrayList<Correction> corrections) {
+        synchronized (this) {
+            gpsConstellation.addCorrections(corrections);
+            galileoConstellation.addCorrections(corrections);
+        }
+    }
+
+    @Override
+    public Time getTime() {
+        synchronized (this) {
+            return gpsConstellation.getTime();
+        }
+    }
+
+    @Override
+    public String getName() {
+        synchronized (this) {
+            return NAME;
+        }
+    }
+
+    public static void registerClass() {
+        register(
+                NAME,
+                GalileoGpsConstellation.class);
+    }
+}
---
+++ app/src/main/java/com/gnssis/rco/gnsstars_gnssisteam/GNSS_PvTMethod/PvtMethod.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2018 TFI Systems
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.gnssis.rco.gnsstars_gnssisteam.GNSS_PvTMethod;
+
+import android.location.Location;
+
+import com.gnssis.rco.gnsstars_gnssisteam.GNSS_Constellations.Constellation;
+import com.gnssis.rco.gnsstars_gnssisteam.GNSS_part.Coordinates;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * Created by Mateusz Krainski on 1/20/2018.
+ * This class is for implementing a generic interface for PVT calculation methods
+ */
+
+public abstract class PvtMethod {
+    /**
+     * Calculates pose based on given parameters
+     * @param constellation satellite constellation object for which the calculations are to
+     *                      be performed
+     * @return calculated pose of the receiver
+     */
+    public abstract Coordinates calculatePose(
+            Constellation constellation
+    );
+
+    private static HashMap<String, Class<? extends PvtMethod>> registeredObjects = new HashMap<>();
+
+    protected static void register(String constellationName, Class<?extends PvtMethod> objectClass) {
+//        if(registeredObjects.containsKey(constellationName))
+//            throw new IllegalArgumentException("This key is already registered! Select a different name!");
+        if(!registeredObjects.containsKey(constellationName))
+            registeredObjects.put(constellationName, objectClass);
+    }
+
+    public static Set<String> getRegistered(){
+        return registeredObjects.keySet();
+    }
+
+    /**
+     * @return Name of the PVT method, which is to be displayed in the UI
+     */
+    public abstract String getName();
+
+    public static Class<? extends PvtMethod> getClassByName(String name) {
+        return registeredObjects.get(name);
+    }
+
+    public abstract double getClockBias();
+
+    private static boolean initialized = false;
+
+    public static void initialize() {
+        if(!initialized) {
+            WeightedLeastSquares.registerClass();
+            StaticExtendedKalmanFilter.registerClass();
+            DynamicExtendedKalmanFilter.registerClass();
+            PedestrianStaticExtendedKalmanFilter.registerClass();
+            initialized = true;
+        }
+    }
+
+    public void startLog(String name) { }
+
+    public void stopLog() { }
+
+    public void logError(double latError, double lonError) { }
+
+    public void logFineLocation(Location location) {}
+}
